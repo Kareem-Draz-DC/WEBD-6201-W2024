e9aab35a74c3bbca3a587eab53d66780a3d54528
Kareem Draz <kareem.draz@durhamcollege.ca>
2024-01-25 15:59:14 -0500

    Completed Tic Tac Toe game, missing winning logic for player O

--- Class-Notes/DOM-Notes/Tic-Tac-Toe/script.js
+++ Class-Notes/DOM-Notes/Tic-Tac-Toe/script.js
@@ -13,12 +13,105 @@
 //   });
 // }
 
+// Fetch the element with a class name of main-container and store it inside of a variable called mainContainer
 const mainContainer = document.querySelector(".main-container");
+// Initialized a new variable called playerTurn to keep track of who's turn it is
+// let or var
+let playerTurn = "X";
 
+// Added a click event listener to our mainContainer element
 mainContainer.addEventListener("click", (event) => {
   console.log("box " + event.target.classList[2] + " was clicked!");
-
-  event.target.innerText = " X";
+  // We are modifying the innerText property of the clicked element (found from the event object) and changing it to either an X or an O depending on who's turn it is
+  if (playerTurn == "X") {
+    event.target.innerText = "X";
+    // We are re-assigning the value of the playerTurn variable to 'toggle' between X and O
+    playerTurn = "O";
+  } else if (playerTurn == "O") {
+    event.target.innerText = "O";
+    playerTurn = "X";
+  }
+  // At the end of every turn, we want our program to check the board and see if we have a winner.
+  checkForWinner();
 });
 
-const playerTurn = "X";
+// const squares = document.getElementsByClassName("box");
+const squares = document.querySelectorAll(".box");
+
+function clearBoard() {
+  squares.forEach((square) => (square.innerText = ""));
+}
+
+function checkForWinner() {
+  // In order to win, we need to find either three X's or three O's in a row in any direction
+  // Start with the X's
+  // console.log(squares);
+  console.log(squares[0].innerText);
+  if (
+    // If all squares in the top row have an innerText property that equal 'X' then we have a winner!
+    squares[0].innerText == "X" &&
+    squares[1].innerText == "X" &&
+    squares[2].innerText == "X"
+  ) {
+    alert(" X won!!! ");
+    clearBoard();
+  } else if (
+    squares[3].innerText == "X" &&
+    squares[4].innerText == "X" &&
+    squares[5].innerText == "X"
+  ) {
+    alert(" X won!!!");
+    clearBoard();
+  } else if (
+    squares[6].innerText == "X" &&
+    squares[7].innerText == "X" &&
+    squares[8].innerText == "X"
+  ) {
+    alert(" X won!!!");
+    clearBoard();
+  } else if (
+    squares[0].innerText == "X" &&
+    squares[3].innerText == "X" &&
+    squares[6].innerText == "X"
+  ) {
+    alert(" X won !!!");
+    clearBoard();
+  } else if (
+    squares[1].innerText == "X" &&
+    squares[4].innerText == "X" &&
+    squares[7].innerText == "X"
+  ) {
+    alert(" X won !!!");
+    clearBoard();
+  } else if (
+    squares[2].innerText == "X" &&
+    squares[5].innerText == "X" &&
+    squares[8].innerText == "X"
+  ) {
+    alert(" X won !!!");
+    clearBoard();
+  } else if (
+    squares[0].innerText == "X" &&
+    squares[4].innerText == "X" &&
+    squares[8].innerText == "X"
+  ) {
+    alert(" X won !!!");
+    clearBoard();
+  } else if (
+    squares[2].innerText == "X" &&
+    squares[4].innerText == "X" &&
+    squares[6].innerText == "X"
+  ) {
+    alert(" X won !!!");
+    clearBoard();
+  }
+}
+
+// let num = 10;
+
+// function newScope() {
+// A limited scoped variable
+//   console.log(num);
+// }
+
+// newScope();
