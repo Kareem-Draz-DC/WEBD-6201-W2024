0ef53aafe704db3b04fb331ea20f834c8983c15e
KareemD123 <kareem.draz@durhamcollege.ca>
2024-03-07 16:47:39 -0500

    AJAX Fetch & Promises

--- Class-Notes/AJAXandAPIs-Notes/AJAX-pokemon-example/script.js
+++ Class-Notes/AJAXandAPIs-Notes/AJAX-pokemon-example/script.js
@@ -32,22 +32,22 @@ console.log("Script is running!");
 
 // var userData = await postgresDatabase.GetUserData() // for example takes 5 seconds
 // Using Async/Await to handle promise
-function fetchUserData(userData) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({ username: "user", password: 123 });
-      // reject({ status: 400, error: "could not find user" });
-    }, 5000);
-  });
-}
+// function fetchUserData(userData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve({ username: "user", password: 123 });
+//       // reject({ status: 400, error: "could not find user" });
+//     }, 5000);
+//   });
+// }
 
-async function handlePromise() {
-  console.log("Initiating call to database...");
-  let promiseResult = await fetchUserData();
-  console.log(promiseResult);
-}
+// async function handlePromise() {
+//   console.log("Initiating call to database...");
+//   let promiseResult = await fetchUserData();
+//   console.log(promiseResult);
+// }
 
-handlePromise();
+// handlePromise();
 
 // The reason we are defining a promise in the format below is to simulate an API/Database call
 // const DBGetUserData = new Promise((resolve, reject) => {
@@ -70,9 +70,61 @@ handlePromise();
 
 // console.log(" I am line 71 and I ran.");
 
-document.getElementById("loadPokemon").addEventListener("click", function () {
-  // Fetch the list of pokemons
-  // REFERENCE: xhr.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
-  // Using FETCH
-  // Display list of pokemons in HTML
-});
+// document.getElementById("loadPokemon").addEventListener("click", function () {
+//   // Fetch the list of pokemons
+//   // Using FETCH and .THEN
+//   let pokemonList = fetch(
+//     "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",
+//     {
+//       method: "GET",
+//     }
+//   )
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((jsonResult) => {
+//       // The scope of the jsonResult variable is limited to only this .then callback function
+//       console.log(jsonResult);
+//       var pokemonString = "";
+//       // Display list of pokemons in HTML
+//       jsonResult.results.forEach((pokemon) => {
+//         pokemonString += `<li><a href=${pokemon.url}>${pokemon.name}</a></li>`;
+//       });
+//       document.getElementById("pokemonList").innerHTML = pokemonString;
+//       return jsonResult;
+//     })
+//     .catch((err) => console.log("We have encountered an error: ", err))
+//     .finally(() => {
+//       console.log("API Call complete.");
+//     });
+// });
+
+// USING FETCH & ASYNC AWAIT
+document
+  .getElementById("loadPokemon")
+  .addEventListener("click", async function () {
+    // Fetch the list of pokemons
+    try {
+      let pokemonList = await fetch(
+        // URL: for ICE -> Store user input in variable called pokemonName
+        // `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+
+        "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+      ); // By Default, it sends a GET HTTP Request
+      var pokemonString = "";
+      // debugger;
+      var jsonResults = await pokemonList.json();
+      jsonResults.results.forEach((pokemon) => {
+        pokemonString += `<li><a href=${pokemon.url}>${pokemon.name}</a></li>`;
+      });
+      document.getElementById("pokemonList").innerHTML = pokemonString;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+// ICE 6: DUE Monday @ 11:59 PM
+// 1. Create a form where the user will be able to type in the name of a pokemon
+// 2. After the user submits the form, send an API GET Request to the pokeapi searching for the details of the pokemon name the user submitted from the form.
+// 3. If the api returns the pokemon details successfully, then display the details of the pokemon in the HTML (i.e. update the body of the HTML to have the pokemon)
+//4 If  the api returns an error that it can't find the pokemon, display the error to the user and ask them to type a different pokemon name
