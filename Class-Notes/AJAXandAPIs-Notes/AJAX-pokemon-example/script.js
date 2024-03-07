860e1dcc12cccf33c355d08f852a3948bcff9829
KareemD123 <kareem.draz@durhamcollege.ca>
2024-03-07 15:56:18 -0500

    AJAX & API Notes

--- Class-Notes/AJAXandAPIs-Notes/AJAX-pokemon-example/script.js
+++ Class-Notes/AJAXandAPIs-Notes/AJAX-pokemon-example/script.js
@@ -1,25 +1,78 @@
 console.log("Script is running!");
 
+// --------------- Asynchronous API Call using XMLHttpRequest --------------------
+
+// document.getElementById("loadPokemon").addEventListener("click", function () {
+//   // We want to create a new XMLHTTPRequest Object to grab the Pokemon API Data
+
+//   console.log("callback function");
+//   var xhr = new XMLHttpRequest();
+
+//   // The line below will create a GET HTTP Request to the pokemon API via the URL
+//   // The call below is ASYNCHRONOUS
+//   xhr.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
+
+//   xhr.onload = function () {
+//     console.log("API has responded!");
+//     //   console.log(this);
+//     var response = JSON.parse(this.responseText);
+//     console.log(response);
+//     var pokemonString = "";
+//     response.results.forEach((pokemon) => {
+//       pokemonString += `<li><a href=${pokemon.url}>${pokemon.name}</a></li>`;
+//     });
+//     document.getElementById("pokemonList").innerHTML = pokemonString;
+//   };
+//   xhr.send();
+// });
+
+// --------------- Asynchronous API Call using fetch --------------------
+
+// Promises
+
+// var userData = await postgresDatabase.GetUserData() // for example takes 5 seconds
+// Using Async/Await to handle promise
+function fetchUserData(userData) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ username: "user", password: 123 });
+      // reject({ status: 400, error: "could not find user" });
+    }, 5000);
+  });
+}
+
+async function handlePromise() {
+  console.log("Initiating call to database...");
+  let promiseResult = await fetchUserData();
+  console.log(promiseResult);
+}
+
+handlePromise();
+
+// The reason we are defining a promise in the format below is to simulate an API/Database call
+// const DBGetUserData = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve({ username: "user", password: 123 });
+//     // reject({ status: 400, error: "could not find user" });
+//   }, 5000);
+// });
+
+// console.log(DBGetUseData);
+// Using .THEN()
+// The code below will WAIT until the promise resolves (either fulfilled or rejected and THEN it will execute the callback function and pass the promise response as an argument to it)
+// DBGetUserData.then((response) => {
+//   console.log("Promise successfully fulfilled: ", response);
+// })
+//   // .then(res => { });
+//   // .then(res => { });
+//   // .then(res => { });
+//   .catch((err) => console.log("Promise error was caught: ", err));
+
+// console.log(" I am line 71 and I ran.");
+
 document.getElementById("loadPokemon").addEventListener("click", function () {
-  // We want to create a new XMLHTTPRequest Object to grab the Pokemon API Data
-
-  console.log("callback function");
-  var xhr = new XMLHttpRequest();
-
-  // The line below will create a GET HTTP Request to the pokemon API via the URL
-  // The call below is ASYNCHRONOUS
-  xhr.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
-
-  xhr.onload = function () {
-    console.log("API has responded!");
-    //   console.log(this);
-    var response = JSON.parse(this.responseText);
-    console.log(response);
-    var pokemonString = "";
-    response.results.forEach((pokemon) => {
-      pokemonString += `<li><a href=${pokemon.url}>${pokemon.name}</a></li>`;
-    });
-    document.getElementById("pokemonList").innerHTML = pokemonString;
-  };
-  xhr.send();
+  // Fetch the list of pokemons
+  // REFERENCE: xhr.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
+  // Using FETCH
+  // Display list of pokemons in HTML
 });
